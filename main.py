a5064a777245fe3e0e24cd8a668d9d6f15b59e70
sny1411 <matteo2004@outlook.fr>
2020-11-12 17:48:52 +0100

    commencement Host

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ import tkinter
 import socket
 
 def clickOnHost():
-    pass
-
+    host, port = ('', 5566)
+    serv = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    serv.bind((host, port))
+    print(f"Le serveur est démarré sur le port {port}")
+    windowMenu.quit()
 def clickOnJoin():
-    pass
+    windowMenu.quit()
 
-window = tkinter.Tk()
-window.title("Mélange mots - ONLINE")
-window_x = 350
-window_y = 150
-window.geometry(f"{window_x}x{window_y}")
-window.resizable(width=False,height=False)
+windowMenu = tkinter.Tk()
+windowMenu.title("Mélange mots - ONLINE")
+windowMenu_x = 350
+windowMenu_y = 150
+windowMenu.geometry(f"{windowMenu_x}x{windowMenu_y}")
+windowMenu.resizable(width=False,height=False)
 
-tkBtnHost = tkinter.Button(window, text="Héberger une partie",command=clickOnHost)
-tkBtnJoin = tkinter.Button(window, text="Rejoindre une partie",command=clickOnJoin)
+tkBtnHost = tkinter.Button(windowMenu, text="Héberger une partie",command=clickOnHost)
+tkBtnJoin = tkinter.Button(windowMenu, text="Rejoindre une partie",command=clickOnJoin)
 
-tkBtnHost.place(x=window_x//2 - 75,y=10, width=150,height=50)
-tkBtnJoin.place(x=window_x//2 - 75,y=70, width=150,height=50)
+tkBtnHost.place(x=windowMenu_x//2 - 75,y=10, width=150,height=50)
+tkBtnJoin.place(x=windowMenu_x//2 - 75,y=70, width=150,height=50)
 
-window.mainloop()
+windowMenu.mainloop()
