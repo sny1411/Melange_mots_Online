c34c5e38c597c2e3f5eeb55a83a6e0827186514f
sny1411 <matteo2004@outlook.fr>
2020-11-14 11:54:01 +0100

    méthodes pour lire la config presque terminé

--- config.py
+++ config.py
@@ -1,10 +1,21 @@
 #coding:utf-8
 
 import pickle
+from tkinter import messagebox
+
 def learnConfig():
+    dicoConfig = {}
     with open("config.txt","r") as config:
-        print(config.readlines())
+        tableauConfig = config.readlines()
+        for lines in tableauConfig:
+            try:
+                linesSplit = lines.split(":")
+                dicoConfig[linesSplit[0]] = linesSplit[1]
+            except:
+                messagebox.showerror("ERREUR","un probleme dans la configuration est survenu !")
+                break
+    return dicoConfig
 
 
 if __name__ == "__main__":
-    learnConfig()
+    print(learnConfig())
