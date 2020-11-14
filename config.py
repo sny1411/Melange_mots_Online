5ccb375ef73fc9ceb458069d10f0f48343e44638
sny1411 <matteo2004@outlook.fr>
2020-11-14 10:22:11 +0100

    petit avancement config

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 #coding:utf-8
 
-import 
+import pickle
+def learnConfig():
+    with open("config.txt","r") as config:
+        print(config.readlines())
+
+
+if __name__ == "__main__":
+    learnConfig()
